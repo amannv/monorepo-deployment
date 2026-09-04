7f458518692d9b73d22bf9219ed483b2fbd197ef
Aman <aman.ver9345@gmail.com>
2026-09-04 12:01:17 +0530

    created a monorepo and created db package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { prisma } from "@repo/database/prisma";
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
